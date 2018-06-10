80f3896091f98fabf68ae44070fd874e58986159
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2018-06-10 16:36:15 -0400

    Finshed all functionality.

--- server.js
+++ server.js
@@ -1,8 +1,56 @@
-const express = require('express');
+'use strict'
 
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 const app = express();
+
+
 app.use(express.static('static'));
 
-app.listen(3000, function () {
-  console.log('App started on port 3000');
+app.use(bodyParser.json());
+
+
+app.get('/:zipCode',(req,res)=>{
+
+      const zip = parseInt(req.params.zipCode);
+      db.collection('zipcodes').find({'Zipcode': zip}).toArray().then(
+          (weather) => {
+
+                /*if(weather.length == 0)
+                {
+                  res.sendStatus(400).json({'error':'Zipcode not in database'});
+                  reject('');
+                }
+                else
+                {
+                    //console.log('in here');
+                    res.sendStatus(200).json(weather[0]);
+                    resolve('');
+                }*/
+              res.json(weather[0]);
+
+          }
+
+
+      ).catch(error => {res.sendStatus(500).json({'Error':error});});
+
+
+});
+
+
+
+let db;
+MongoClient.connect('mongodb://abe:PanasoniC1@ds247170.mlab.com:47170/zip_codes').then( connection => {
+
+    db= connection.db('zip_codes');
+    app.listen(3000, function () {
+      console.log('App started on port 3000');
+    });
+
+}).catch(error => {
+
+  console.log('Error: ',error);
+
+
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,205 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactAnimatedWeather from 'react-animated-weather';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-//import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-//import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import FlatButton from 'material-ui/FlatButton';
-import TextField from 'material-ui/TextField';
-import AppBar from 'material-ui/AppBar';
-import {Line} from 'react-chartjs-2';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
-import {Tabs, Tab} from 'material-ui/Tabs';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 
-/*
-const icon =[
+import Moment from 'react-moment';
 
-   'CLEAR_DAY',
-    'CLEAR_NIGHT',
-   'PARTLY_CLOUDY_DAY',
-   'PARTLY_CLOUDY_NIGHT',
-   'CLOUDY',
-   'RAIN',
-   'SLEET',
-   'SNOW',
-   'WIND',
-   'FOG'
+
+const defaults = {
+  icon: 'CLEAR_DAY',
+  color: 'inherit',
+  size: 64,
+  animate: true
 };
-*/
 
-// 200,300,500s use Rain
-// 600s use snow
-//700s use FOG
-// 800 use  clear day/ clear night
-//80x use clouds
-/*
-swtch(int)
+
+const dict =
 {
+  'clear-day':'CLEAR_DAY',
+  'clear-night':'CLEAR_NIGHT',
+  'partly-cloudy-day':'PARTLY_CLOUDY_DAY',
+  'partly-cloudy-night':'PARTLY_CLOUDY_NIGHT',
+  'cloudy':'CLOUDY',
+  'rain':'RAIN',
+  'sleet':'SLEET',
+  'snow':'SNOW',
+  'wind':'WIND',
+  'fog':'FOG',
+  'hail':'RAIN',
+  'thunderstorm':'RAIN',
+  'tornado':'WIND'
+};
 
 
-
-}
-*/
-
-/*
-class WeatherDay extends React.Component{
-
-  constructor(props)
-  {
-    super(props);
-    this.state{
-      day: [],
-      time: [],
-      temp: [],
-      tempMin: [],
-      tempMax: [],
-      humidity :[],
-      rainChance: [],
-      weather:[],
-      clouds:[],
-      wind: []
-
-    };
-  }
-
-
-}
-
-var timestamp = 654524560; // UNIX timestamp in seconds
-var xx = new Date();
-xx.setTime(timestamp*1000); // javascript timestamps are in milliseconds
-
-*/
-
-function kToF(kel)
-{
-  return kel * (9/5) - 459.67;
-}
-//<TableRowColumn>{props.datum.dt_txt}</TableRowColumn>
 const WeatherRow = (props) => (
   <TableRow >
-
-  <TableRowColumn>{props.datum.dt_txt}</TableRowColumn>
-    <TableRowColumn>{kToF(props.datum.main.temp.toFixed(1))}</TableRowColumn>
-    <TableRowColumn>{props.datum.main.temp_min}/{props.datum.main.temp_max}</TableRowColumn>
-    <TableRowColumn>{props.datum.main.humidity}%</TableRowColumn>
-    <TableRowColumn>{props.datum.weather[0].description}</TableRowColumn>
-    <TableRowColumn>{props.datum.wind.speed*2.24}</TableRowColumn>
-
+  <TableCell><Moment unix format="MM/DD/YYYY">{props.datum.time}</Moment></TableCell>
+    <TableCell>{props.datum.apparentTemperatureHigh}</TableCell>
+    <TableCell>{props.datum.apparentTemperatureHigh}/{props.datum.apparentTemperatureLow}</TableCell>
+    <TableCell>{props.datum.humidity}%</TableCell>
+    <TableCell>{props.datum.summary}</TableCell>
+    <TableCell>{props.datum.windSpeed}</TableCell>
+    <TableCell>
+        <ReactAnimatedWeather
+            icon={dict[props.datum.icon]}
+            color={defaults.color}
+            size={defaults.size}
+            animate={defaults.animate}/>
+    </TableCell>
   </TableRow>
 )
 
 
-
 function WeatherTable(props) {
-//  const weatherRows = props.weatherData.map(datum => <WeatherRow  datum={datum} />)
+  const weatherRows = props.weatherData.map(datum => <WeatherRow  datum={datum} />);
 
-
-
-  //need to separate based on day
-
-  let days = [[],[],[],[],[]];
-
-//about 8 results per day
-let weatherRows = 0;
-let weatherRows2 = 0;
-
-
-if(props.weatherData.length !=0)
-{
-  for(var numOfDays= 0; numOfDays < 5;numOfDays++)
-  {
-
-        let singleDay = props.weatherData[numOfDays*8].dt_txt.substring(0,10); // yyyy-mm-dd
-
-        for(var i = 0; i < 7; i++)
-        {
-            let v = props.weatherData[i+ numOfDays*8].dt_txt.substring(0,10);
-                if( v === singleDay)
-                {
-                    days[numOfDays].push(props.weatherData[i+ numOfDays*8]);
-                  }
-
-        }
-  }
-
-    weatherRows = days[0].map(datum => <WeatherRow  datum={datum} />);
-
-    weatherRows2 = {
-      labels: [0,1,2,3,4,5,6],
-      datasets: [
-          {
-            label: 'Todays weather',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [days[0][0].main.temp,days[0][1].main.temp,days[0][2].main.temp,days[0][3].main.temp,
-                  days[0][4].main.temp,days[0][5].main.temp,days[0][6].main.temp
-          ]
-
-          }]
-
-
-      };
-
-
-}
-  //const
-    //const weatherRows1 = days[0].map(datum => <WeatherRow  datum={datum} />);
-  //    const weatherRows3 = days[0].map(datum => <WeatherRow  datum={datum} />);
-      //  const weatherRows4 = days[0].map(datum => <WeatherRow  datum={datum} />);
-      //    const weatherRows5 = days[0].map(datum => <WeatherRow  datum={datum} />);
-  //
    return (
      <div>
           <Table>
-            <TableHeader>
+            <TableHead>
               <TableRow>
-
-                <TableHeaderColumn>Day</TableHeaderColumn>
-                <TableHeaderColumn>Temperature</TableHeaderColumn>
-                <TableHeaderColumn>High/Low</TableHeaderColumn>
-                <TableHeaderColumn>Humidity</TableHeaderColumn>
-                <TableHeaderColumn>Description</TableHeaderColumn>
-                <TableHeaderColumn>Wind MPH</TableHeaderColumn>
-
+                <TableCell>Day</TableCell>
+                <TableCell>Temperature</TableCell>
+                <TableCell>High/Low</TableCell>
+                <TableCell>Humidity</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Wind MPH</TableCell>
+                <TableCell>ICON</TableCell>
               </TableRow>
-
-            </TableHeader>
+            </TableHead>
             <TableBody>{weatherRows}</TableBody>
-
           </Table>
-
-            <Line data={weatherRows2} />
-
-          </div>
-
+     </div>
   );
 }
-
 
 
 
@@ -208,9 +94,35 @@ class GetWeather extends React.Component {
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getLonLatFromZip = this.getLonLatFromZip.bind(this);
+    this.state = {weatherData: [], zipCodeErrorText: ''};
 
-    this.state = {weatherData: []};
+  }
 
+   getLonLatFromZip(zip)
+  {
+    return new Promise((resolve,reject)=>{
+
+      let serverUrl = '/' + zip;
+      let cityDataRequest = new XMLHttpRequest();
+      cityDataRequest.open('GET',serverUrl,true);
+
+       cityDataRequest.onload = function() {
+         if (this.readyState === 4 && this.status === 200) {
+            // alert(JSON.parse(this.responseText));
+           let response = JSON.parse(this.responseText);
+
+           resolve({'Lat': response.Lat, 'Long': response.Long});
+
+         }
+         else {
+           {
+             reject('Could not find zip code');
+           }
+         }
+       }
+     cityDataRequest.send();
+    });
   }
 
   handleSubmit(e) {
@@ -218,33 +130,43 @@ class GetWeather extends React.Component {
     var form = document.forms.checkWeather;
 
     let city = form.zipcode.value;
-    let request = new XMLHttpRequest();
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c3af11a2c8f2b862ff1cd62e0f348861`;
-    let response = 0;
-    let self = this;
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-       response = JSON.parse(this.responseText);
+    //need to get lat and long data from server
+      let self = this;
+    this.getLonLatFromZip(city).then((result)=>{
 
-        self.setState({weatherData: response.list});
-        //alert(`The humidity in ${city} is ${response.list[0].main.humidity} and temp is ${response.list[0].main.temp}`);
-      //  console.log(`${response}`);
 
+      let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/41f274b696fa65192e89506d2c1c5fc4/${result.Lat},${result.Long}?exclude=currently
+        ,minutely,hourly`;
+        let request =  new XMLHttpRequest();
+        request.open('GET', url, true);
+
+
+      request.onload = function() {
+        if (this.readyState === 4 && this.status === 200) {
+
+          let response = JSON.parse(this.responseText);
+
+          self.setState({weatherData: response.daily.data,zipCodeErrorText:''});
+
+
+        }
       }
-    }
-
-    request.open("GET", url, true);
     request.send();
 
 
+
+  }).catch(err=>{
+
+    self.setState({zipCodeErrorText:err});
+  });
   }
 
   render() {
     return (
       <div>
-        <form name="checkWeather" onSubmit={this.handleSubmit}>
-          <TextField type="text" name="zipcode" placeholder="Zip Code" />
-          <FlatButton  onClick={this.handleSubmit}>Get Weather</FlatButton  >
+        <form style={{textAlign:'center'}} name="checkWeather" onSubmit={this.handleSubmit}>
+          <TextField errorText={this.state.zipCodeErrorText} autofocus="true" type="text" name="zipcode" placeholder="Zip Code" />
+          <Button  onClick={this.handleSubmit}>Get Weather</Button >
         </form>
         <WeatherTable weatherData={this.state.weatherData} />
       </div>
@@ -254,32 +176,26 @@ class GetWeather extends React.Component {
 
 
 
-
-const defaults = {
-  icon: 'CLEAR_DAY',
-  color: 'goldenrod',
-  size: 64,
-  animate: true
-};
-
 class MainComponent extends React.Component {
   constructor() {
     super();
-  //  this.state = { issues: [] };
-
   }
 
-//  <ReactAnimatedWeather icon={defaults.icon} color={defaults.color} size={defaults.size} animate={defaults.animate} />
+
 
   render() {
     return (
-
       <div>
-      <MuiThemeProvider>
-          <AppBar title ="Weather Tracker" style={{textAlign:'center'}} />
-          <GetWeather />
-
-      </MuiThemeProvider>
+          <MuiThemeProvider>
+              <AppBar position="static" style={{textAlign:'center'}} >
+                  <Toolbar>
+                      <Typography  variant="title" color="inherit">
+                          Weather Tracker
+                      </Typography>
+                  </Toolbar>
+              </AppBar>
+              <GetWeather />
+          </MuiThemeProvider>
       </div>
 
 
